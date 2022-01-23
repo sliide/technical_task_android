954dd18a3cc9cf8d7160b976f2d7f34c9edd8042
Sachin Personal <sachinsapkale87@gmail.com>
2022-01-24 02:07:16 +0530

    comment on network code

--- app/src/main/java/com/sachinsapkale/android_test_maverick/utils/NetworkValidation.kt
+++ app/src/main/java/com/sachinsapkale/android_test_maverick/utils/NetworkValidation.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.net.ConnectivityManager
 
 
-fun isNetworkAvailable(context: Context?): Boolean {
+fun isNetworkAvailable(context: Context?): Boolean { // using old deprecated method as dont feel writing code for < Android M and other is not necessary now.
         val connectivityManager =
             context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         return connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!
